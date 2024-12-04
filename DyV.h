1e2e7711149077627addd01976a7cdc4a269a3b7
dafnee17 <dacebosp@epsa.upv.es>
2024-12-04 02:44:44 +0100

    Implementación de QuickSort con variantes de pivote y pruebas

--- DyV.h
+++ DyV.h
@@ -2,8 +2,13 @@
 #define DYV_H
 
 #include <vector>
+#include <algorithm>
+#include <cstdlib> // Para la función rand
 
-// Función plantilla para búsqueda binaria en vectores ascendentes
+// -----------------------------
+// Búsqueda Binaria Ascendente
+// -----------------------------
+
 template <typename T>
 int BusquedaBinaria(T x, const std::vector<T>& v, int ini, int fin) {
     if (ini > fin) {
@@ -21,7 +26,10 @@ int BusquedaBinaria(T x, const std::vector<T>& v, int ini, int fin) {
     }
 }
 
-// Función plantilla para búsqueda binaria en vectores descendentes
+// -----------------------------
+// Búsqueda Binaria Descendente
+// -----------------------------
+
 template <typename T>
 int BusquedaBinaria_INV(T x, const std::vector<T>& v, int ini, int fin) {
     if (ini > fin) {
@@ -37,6 +45,50 @@ int BusquedaBinaria_INV(T x, const std::vector<T>& v, int ini, int fin) {
     } else {
         return BusquedaBinaria_INV(x, v, medio + 1, fin); // Busca en la parte derecha
     }
+}
+
+// -----------------------------
+// QuickSort con particiones
+// -----------------------------
+
+template <typename T, typename PartitionFunc>
+void QuickSort(std::vector<T>& v, int ini, int fin, PartitionFunc partitionFunc) {
+    if (ini < fin) {
+        int pivot = partitionFunc(v, ini, fin);
+        QuickSort(v, ini, pivot - 1, partitionFunc);
+        QuickSort(v, pivot + 1, fin, partitionFunc);
+    }
+}
+
+// Función de partición: pivote como el primer elemento
+template <typename T>
+int Partition_First(std::vector<T>& v, int ini, int fin) {
+    T pivot = v[ini];
+    int i = ini + 1;
+    for (int j = ini + 1; j <= fin; ++j) {
+        if (v[j] < pivot) {
+            std::swap(v[i], v[j]);
+            i++;
+        }
+    }
+    std::swap(v[ini], v[i - 1]);
+    return i - 1;
+}
+
+// Función de partición: pivote aleatorio
+template <typename T>
+int Partition_Random(std::vector<T>& v, int ini, int fin) {
+    int randomIndex = ini + rand() % (fin - ini + 1);
+    std::swap(v[ini], v[randomIndex]);
+    return Partition_First(v, ini, fin); // Reutilizamos Partition_First
+}
+
+// Función de partición: pivote central
+template <typename T>
+int Partition_Mid(std::vector<T>& v, int ini, int fin) {
+    int midIndex = ini + (fin - ini) / 2;
+    std::swap(v[ini], v[midIndex]);
+    return Partition_First(v, ini, fin); // Reutilizamos Partition_First
 }
 
 #endif // DYV_H
